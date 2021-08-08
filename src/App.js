f4e9357be439c872309c747be9c660f5f8b9e429
Caio Geraldes <caioaguida@gmail.com>
2021-08-08 12:45:32 -0300

    fix 9

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import Select from 'react-select';
 
-import pie from './converters/pie.js';
-import polygreek from './converters/polytonicgreek.js';
-import linearb from './converters/linearb.js';
-import cypriot from './converters/cypriot.js';
-import armenian from './converters/armenian.js';
-import avestan from './converters/avestan.js';
-import avestanTrans from './converters/avestanTrans.js';
-import oldpersian from './converters/oldpersian.js';
-import gothic from './converters/gothic.js';
-import hittite from './converters/hittite.js';
-import luwian from './converters/luwian.js';
-import lycian from './converters/lycian.js';
-import lydian from './converters/lydian.js';
-import carian from './converters/carian.js';
-import ogham from './converters/ogham.js';
-import oscan from './converters/oscan.js';
+import pie from '/converters/pie.js';
+import polygreek from '/converters/polytonicgreek.js';
+import linearb from '/converters/linearb.js';
+import cypriot from '/converters/cypriot.js';
+import armenian from '/converters/armenian.js';
+import avestan from '/converters/avestan.js';
+import avestanTrans from '/converters/avestanTrans.js';
+import oldpersian from '/converters/oldpersian.js';
+import gothic from '/converters/gothic.js';
+import hittite from '/converters/hittite.js';
+import luwian from '/converters/luwian.js';
+import lycian from '/converters/lycian.js';
+import lydian from '/converters/lydian.js';
+import carian from '/converters/carian.js';
+import ogham from '/converters/ogham.js';
+import oscan from '/converters/oscan.js';
 import { hk_deva, hk_iast, hk_iso } from './converters/vedic.js';
 import { glagolitic } from './converters/glagolitic.js';
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import '/index.css';
+import App from '/App';
 import reportWebVitals from './reportWebVitals';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
