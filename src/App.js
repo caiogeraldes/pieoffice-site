b4700d49dbbb8e8944f135a053ef99035407b529
Caio Geraldes <caioaguida@protonmail.com>
2021-08-10 09:46:53 -0300

    displace converters to npm module

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import Select from 'react-select';
 
-import pie from './converters/pie.js';
-import polygreek from './converters/polytonicgreek.js';
-import linearb from './converters/linearb.js';
-import cypriot from './converters/cypriot.js';
-import armenian from './converters/armenian.js';
-import avestan from './converters/avestan.js';
-import avestanTrans from './converters/avestanTrans.js';
-import oldpersian from './converters/oldpersian.js';
-import gothic from './converters/gothic.js';
-import hittite from './converters/hittite.js';
-import luwian from './converters/luwian.js';
-import lycian from './converters/lycian.js';
-import lydian from './converters/lydian.js';
-import carian from './converters/carian.js';
-import ogham from './converters/ogham.js';
-import oscan from './converters/oscan.js';
-import { hk_deva, hk_iast, hk_iso } from './converters/vedic.js';
-import { glagolitic } from './converters/glagolitic.js';
+import { //pie,
+    // polygreek,
+    // linearb,
+    // cypriot,
+    // armenian,
+    // avestan,
+    // avestanTrans,
+    // oldpersian,
+    // gothic,
+    // hittite,
+    // luwian,
+    // lycian,
+    // lydian,
+    // carian,
+    // ogham,
+    // oscan,
+    // hk_deva,
+    // hk_iast,
+    // hk_iso,
+    // glagolitic,
+    allConverters } from "pieoffice";
 
 
 function App() {
@@ -58,30 +61,6 @@ function TextOut() {
     );
 }
 
-
-const options = [
-  { value: 'pie', label: 'Proto-Indo-European' },
-  { value: 'polygreek', label: 'Polytonic Greek' },
-  { value: 'linearb', label: 'Linear B' },
-  { value: 'cypriot', label: 'Cypriot Syllabary' },
-  { value: 'armenian', label: 'Armenian' },
-  { value: 'vedicdeva', label: 'Vedic / Sanskrit (Devanāgarī)'},
-  { value: 'vediciast', label: 'Vedic / Sanskrit (IAST)'},
-  { value: 'vediciso', label: 'Vedic / Sanskrit (ISO)'},
-  { value: 'avestan', label: 'Avestan (Script)' },
-  { value: 'avestanTrans', label: 'Avestan (Translit)' },
-  { value: 'oldpersian', label: 'Old Persian Cuneiform' },
-  { value: 'gothic', label: 'Gothic' },
-  { value: 'hittite', label: 'Hittite (Incomplete, only Van den Hout)' },
-  { value: 'luwian', label: 'Hieroglyphic Luwian' },
-  { value: 'lydian', label: 'Lydian' },
-  { value: 'lycian', label: 'Lycian' },
-  { value: 'carian', label: 'Carian' },
-  { value: 'ogham', label: 'Ogham' },
-  { value: 'oscan', label: 'Oscan' },
-  { value: 'glagolitic', label: 'Glagolitic' }
-]
-
 var converter = (input) => {return input};
 
 function convertHandler (e) {
@@ -92,57 +71,15 @@ function convertHandler (e) {
 
 const LangSelect = () => {
 
-    var lang = "";
     const handleChange = (e) => {
-        lang = e.value;
-        if (lang === "pie") {
-            converter = pie;
-        } else if (lang === "linearb") {
-            converter = linearb;
-        } else if (lang === "cypriot") {
-            converter = cypriot;
-        } else if (lang === "armenian") {
-            converter = armenian;
-        } else if (lang === "vedicdeva") {
-            converter = hk_deva;
-        } else if (lang === "vediciast") {
-            converter = hk_iast;
-        } else if (lang === "vediciso") {
-            converter = hk_iso;
-        } else if (lang === "avestan") {
-            converter = avestan;
-        } else if (lang === "avestanTrans") {
-            converter = avestanTrans;
-        } else if (lang === "oldpersian") {
-            converter = oldpersian;
-        } else if (lang === "gothic") {
-            converter = gothic;
-        } else if (lang === "hittite") {
-            converter = hittite;
-        } else if (lang === "luwian") {
-            converter = luwian;
-        } else if (lang === "lycian") {
-            converter = lycian;
-        } else if (lang === "lydian") {
-            converter = lydian;
-        } else if (lang === "carian") {
-            converter = carian;
-        } else if (lang === "ogham") {
-            converter = ogham;
-        } else if (lang === "oscan") {
-            converter = oscan;
-        } else if (lang === "glagolitic") {
-            converter = glagolitic;
-        } else if (lang === "polygreek") {
-             converter = polygreek
-        }
+            converter = e.converter;
     }
 
     return (
         <Select
             placeholder={"Select a script"}
             onChange={handleChange}
-            options={options}
+            options={allConverters}
         />
     );
 }
